9afd1787f277ade6c919ca84203f07bea486ef80
Fink <ravenhollm@gmail.com>
2011-01-19 11:28:47 +0200

    forvardPage method added to ServletUtil class

--- src/main/java/com/exadel/friendface/servletutil/ServletUtil.java
+++ src/main/java/com/exadel/friendface/servletutil/ServletUtil.java
@@ -19,15 +19,22 @@ import java.io.IOException;
 
 public class ServletUtil {
 
-        public static void forvardErrorPage(HttpServlet servlet, HttpServletRequest request,
-                                  HttpServletResponse response, String message)
-
-	        throws ServletException, IOException {
-
-    	ServletContext sc = servlet.getServletContext();
+    public static void forvardErrorPage(HttpServlet servlet, HttpServletRequest request,
+                                        HttpServletResponse response, String message)
+            throws ServletException, IOException {
+        ServletContext sc = servlet.getServletContext();
     	RequestDispatcher rd = sc.getRequestDispatcher("/pages/errorpages/wrongdata.jsp");
     	request.setAttribute("message", message);
     	rd.forward(request, response);
     }
 
+    public static void forvardPage(HttpServlet servlet, HttpServletRequest request,
+                                        HttpServletResponse response, String message,
+                                        String url)
+            throws ServletException, IOException {
+        ServletContext sc = servlet.getServletContext();
+    	RequestDispatcher rd = sc.getRequestDispatcher(url);
+    	request.setAttribute("message", message);
+    	rd.forward(request, response);
+    }
 }
