29427ccdfe4c9c1b30efa0fc7c1ec9bc0551da92
ravenhollm@gmail.com <@.(none)>
2011-02-26 22:34:09 +0200

    few dummy action classes added. i18n refactored.

---
+++ src/main/java/com/exadel/friendface/actions/Entrance.java
@@ -0,0 +1,16 @@
+package com.exadel.friendface.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Author: S. Fink
+ * Date: 26.02.11
+ * Time: 22:27
+ */
+
+public class Entrance extends ActionSupport {
+    @Override
+    public String execute() throws Exception {
+        return SUCCESS;
+    }
+}
