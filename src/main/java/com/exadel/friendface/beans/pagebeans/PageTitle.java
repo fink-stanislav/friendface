6db529339163faec42d2c4246ff16000e8220948
ravenhollm@gmail.com <@.(none)>
2011-02-27 17:02:08 +0200

    i18n page titles provided.

---
+++ src/main/java/com/exadel/friendface/beans/pagebeans/PageTitle.java
@@ -0,0 +1,21 @@
+package com.exadel.friendface.beans.pagebeans;
+
+import java.io.Serializable;
+
+/**
+ * Author: S. Fink
+ * Date: 2/27/11
+ * Time: 4:22 PM
+ */
+
+public class PageTitle implements Serializable {
+    private String bundleName;
+
+    public String getBundleName() {
+        return bundleName;
+    }
+
+    public void setBundleName(String bundleName) {
+        this.bundleName = bundleName;
+    }
+}
