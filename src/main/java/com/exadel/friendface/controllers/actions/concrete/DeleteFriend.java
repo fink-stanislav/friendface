8c3942573958dfac804cc44f3990b15c58626db3
Fink Stanislav <ravenhollm@gmail.com>
2011-03-08 14:27:49 +0200

    search page added. style refactored.

--- src/main/java/com/exadel/friendface/controllers/actions/concrete/DeleteFriend.java
+++ src/main/java/com/exadel/friendface/controllers/actions/concrete/DeleteFriend.java
@@ -1,10 +1,7 @@
 package com.exadel.friendface.controllers.actions.concrete;
 
 import com.exadel.friendface.controllers.actions.StrutsAction;
-import com.exadel.friendface.model.dao.DAOFactory;
-import com.exadel.friendface.model.dao.FriendsDAO;
 import com.exadel.friendface.model.entities.Friend;
-import com.exadel.friendface.service.FriendfaceService;
 
 import static com.exadel.friendface.service.FriendfaceService.getService;
 
@@ -28,7 +25,7 @@ public class DeleteFriend extends StrutsAction {
     @Override
     public String execute() {
         try {
-            Friend friend = getService().getFriendsService().find(id);
+            Friend friend = getService().getFriendsService().getById(id);
             getService().getFriendsService().remove(friend);
             return SUCCESS;
         } catch (Exception e) {
